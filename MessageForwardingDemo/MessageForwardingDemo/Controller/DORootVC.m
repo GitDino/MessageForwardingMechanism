1390abd04840dea0edb7e45dde54ea75474c4d65
Dino <kill_death@163.com>
2018-04-11 22:28:09 +0800

    Demo初步搭建

---
+++ MessageForwardingDemo/MessageForwardingDemo/Controller/DORootVC.m
@@ -0,0 +1,24 @@
+//
+//  DORootVC.m
+//  MessageForwardingDemo
+//
+//  Created by 魏欣宇 on 2018/4/11.
+//  Copyright © 2018年 Dino. All rights reserved.
+//
+
+#import "DORootVC.h"
+
+@interface DORootVC ()
+
+@end
+
+@implementation DORootVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"消息转发机制";
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+@end
